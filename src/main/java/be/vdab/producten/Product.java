9a1627367a8f82ed00a0d702c746bc1df80e065a
yuliyaBir <zhivo4ka@gmail.com>
2024-01-23 11:46:37 +0100

    Initial commit

---
+++ src/main/java/be/vdab/producten/Product.java
@@ -0,0 +1,2 @@
+package be.vdab.producten;public class Product {
+}
---
+++ src/main/java/be/vdab/producten/ProductenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.producten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductenApplication.class, args);
+	}
+
+}
