02c55c5c773b4efdac6a736147736a87a7ee3689
yuliyaBir <zhivo4ka@gmail.com>
2024-02-13 15:20:18 +0100

    autoscaler.yaml

--- src/main/java/be/vdab/producten/ProductController.java
+++ src/main/java/be/vdab/producten/ProductController.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class ProductController {
@@ -13,6 +15,11 @@ public class ProductController {
     @GetMapping("/")
     List<Product> findAll(){
         logger.info("GET request naar alle producten");
+        var random = new Random();
+        var totaal = BigDecimal.ZERO;
+        for (var teller = 0; teller < 7_000_000; teller++) {
+            totaal = totaal.add(BigDecimal.valueOf(random.nextDouble()));
+        }
         return List.of(
                 new Product(1, "appel"),
                 new Product(2, "peer"),
